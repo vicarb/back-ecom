53d1c45b52870b11130ca78915898bb3283c9a4f
vicarb <vcardenasb@protonmail.com>
2023-06-01 08:56:29 -0400

    updated cart service

--- src/cart/cart.service.ts
+++ src/cart/cart.service.ts
@@ -11,19 +11,35 @@ export class CartService {
     const product = await this.productsService.findOne(productId);
     // Verify product availability and other business rules here
     // Add product to user's cart in the data store
+    const cartItem = this.cart.get(userId) || [];
+    cartItem.push({ product, quantity });
+    this.cart.set(userId, cartItem);
   }
 
   async removeProduct(userId: string, productId: string) {
-    // Implementation depends on your data store.
+    const cartItem = this.cart.get(userId);
+    if (!cartItem) return;
+    const index = cartItem.findIndex(item => item.product.id === productId);
+    if (index > -1) {
+      cartItem.splice(index, 1);
+      this.cart.set(userId, cartItem);
+    }
   }
 
   async updateProductQuantity(userId: string, productId: string, quantity: number) {
     const product = await this.productsService.findOne(productId);
     // Verify product availability and other business rules here
     // Update product quantity in user's cart in the data store
+    const cartItem = this.cart.get(userId);
+    if (!cartItem) return;
+    const item = cartItem.find(item => item.product.id === productId);
+    if (item) {
+      item.quantity = quantity;
+    }
   }
 
   getCart(userId: string) {
     // Implementation depends on your data store.
+    return this.cart.get(userId) || [];
   }
 }
